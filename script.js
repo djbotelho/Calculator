95ac42b6a020c01a4fb49c2d9c9ae14800dfbb37
Daniel Botelho <botelhodaniel1998@gmail.com>
2024-07-12 11:11:08 +0100

    Final push
    The screen clears when pressing buttons that are not numbers, but the calculations work fine

--- script.js
+++ script.js
@@ -2,9 +2,49 @@ let currentInput = "";
 let operator = "";
 let secondInput = "";
 let newInput = "";
-
 let display = document.getElementById("result");
 
+document.addEventListener("keydown", e => {
+    if (e.code === "Numpad0" || e.code === "Digit0") {
+      displayValue(0);
+    } else if (e.code === "Numpad1" || e.code === "Digit1") {
+      displayValue(1);
+    } else if (e.code === "Numpad2" || e.code === "Digit2") {
+      displayValue(2);
+    } else if (e.code === "Numpad3" || e.code === "Digit3") {
+      displayValue(3);
+    } else if (e.code === "Numpad4" || e.code === "Digit4") {
+      displayValue(4);
+    } else if (e.code === "Numpad5" || e.code === "Digit5") {
+      displayValue(5);
+    } else if (e.code === "Numpad6" || e.code === "Digit6") {
+      displayValue(6);
+    } else if (e.code === "Numpad7" || e.code === "Digit7") {
+      displayValue(7);
+    } else if (e.code === "Numpad8" || e.code === "Digit8") {
+      displayValue(8);
+    } else if (e.code === "Numpad9" || e.code === "Digit9") {
+      displayValue(9);
+    } else if (e.code === "Period") {
+      
+      if (!currentInput.includes(".")) {
+        currentInput += ".";
+        display.value = currentInput;
+      }
+    } else if (e.code === "Backspace") {
+      clearValue();
+    } else if (e.code === "Enter" || e.code === "NumpadEnter") {
+      calcular();
+    } else if (e.code === "NumpadAdd" || e.code === "Plus") {
+      operate("+");
+    } else if (e.code === "NumpadSubtract" || e.code === "Minus") {
+      operate("-");
+    } else if (e.code === "NumpadMultiply" || e.code === "Multiply") {
+      operate("*");
+    } else if (e.code === "NumpadDivide" || e.code === "Slash") {
+      operate("/");
+    }
+  });
 function displayValue(value) {
   currentInput += value;
   display.value = currentInput;
@@ -70,4 +110,11 @@ function quadrado() {
 function current() {
   alert(currentInput);
   alert(typeof currentInput);
+}
+
+function decimal() {
+
+    currentInput += ".";
+    
+
 }
